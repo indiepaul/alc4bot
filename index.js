ac42c45bbad91f26488b6ca4efed8389749b7116
Paul Mtali <paul.mtali@techie.com>
2019-07-15 10:59:27 +0200

    tweet mentions

--- index.js
+++ index.js
@@ -6,43 +6,48 @@ const schedule = require('node-schedule');
 dotenv.config();
 
 const T = new Twit({
-  consumer_key:         process.env.CONSUMER_KEY,
-  consumer_secret:      process.env.CONSUMER_SECRET,
-  access_token:         process.env.ACCESS_TOKEN,
-  access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
-  timeout_ms:           60 * 1000,
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  timeout_ms: 60 * 1000,
 });
 
-const stream = T.stream('statuses/filter', { track: ['#150daysofalc4'] });
+const stream = T.stream('statuses/filter', { track: ['#150daysofalc4', '@150DaysOfALC4'] });
+console.log('listening');
 
 stream.on('tweet',
   tweet => {
-    console.log('tweet received! ', tweet.text)
-    T.post('statuses/retweet/:id', {
+    if (tweet.user.id_str !== '1142546386187182088') {
+      console.log('tweet received! ', tweet.text)
+      T.post('statuses/retweet/:id', {
         id: tweet.id_str
-    }, function (err, response) {
+      }, function (err, response) {
         if (response) {
-            console.log('Retweeted!!!');
+          console.log('Retweeted!!!');
         }
         if (err) {
-            console.log(err);
-            console.log('Problem when retweeting. Possibly already retweeted this tweet!');
+          // console.log(err);
+          console.log('Problem when retweeting. Possibly already retweeted this tweet!');
         }
-    });
+      });
+    }
   }
 );
-var motivate = async function() {
-    const res = await p({
-        'url': 'http://10.150.35.18/quotes.rest/qod.json?category=inspire',
-        'parse': 'json'
-    })
-    const quote = res.body.contents.quotes[0]
-    var tweet = "\"" + quote.quote + "\"\r\n - " + quote.author;
-    T.post('statuses/update', { status: tweet }, function(err, data, response) {
-        console.log(data.text)
-    });
+var motivate = async function () {
+  const res = await p({
+    'url': 'http://10.150.35.18/quotes.rest/search.json?category=inspire',
+    'parse': 'json'
+  })
+  const quote = res.body.contents.quotes[0]
+  var tweet = "\"" + quote.quote + "\"\r\n - " + quote.author;
+  T.post('statuses/update', { status: tweet }, function (err, data, response) {
+    console.log(data.text)
+  });
 }
 
-var j = schedule.scheduleJob('* * 9 * *', function(){
-    motivate();
+var rule = new schedule.RecurrenceRule();
+rule.minute = 0;
+var j = schedule.scheduleJob(rule, function () {
+  motivate();
 });
